8bfc45d3d4b2ec7cfcea9c291db87b48a346e018
Jon Vinson <Jon Vinson>
2016-08-14 14:30:52 -0500

    added fetch api and supporting libs

--- Scripts/Http.ts
+++ Scripts/Http.ts
@@ -1,5 +1,7 @@
 /// <reference path="../typings/es6-promise/es6-promise.d.ts" />
 
+declare var fetch:any;
+
 export interface IHttpService {
     get<T>(url: string): Promise<IHttpResponse<T>>;
 }
@@ -8,4 +10,20 @@ export interface IHttpResponse<T> {
     status: number;
     statusText: string;
     data: T;
+}
+
+export class FetchHttpService implements IHttpService {
+    get<T>(url: string): Promise<IHttpResponse<T>> {
+        return new Promise<IHttpResponse<T>>((resolve, reject) => {
+            fetch(url)
+            .then(response => {
+                return response.json().then(data => {
+                    resolve({ status: response.status, 
+                        statusText: response.statusText, 
+                        data: data
+                    })
+                });
+            });
+        });
+    }
 }
